34c8f6750068a94988e31464e3ea8ba04410f98a
Demiu <miazgapow@gmail.com>
2021-03-26 00:26:55 +0100

    Implement signal line calculation

--- macd.py
+++ macd.py
@@ -21,9 +21,9 @@ def ema_numerator(samples: list[float], alpha: float) -> tuple[float, list[float
     value = sum(terms)
     return (value,terms)
 
-# returns a tuple of the EMA numerator and the list of terms used to create it
 # 0th sample is p0 in the formula
 # takes the array of terms used to calculate the previous numerator (to speed up calculations) and the newest sample
+# returns: a tuple of the EMA numerator and the list of terms used to create it
 def ema_numerator_next(sample: float, alpha: float, prev_array: list[float]) -> tuple[float, list[float]]:
     # remove last sample, increase the exponent for (1-alpha) for all past samples
     past_samples = [(x * (1-alpha)) for x in prev_array[:-1]]
@@ -34,6 +34,7 @@ def ema_numerator_next(sample: float, alpha: float, prev_array: list[float]) -> 
 
 # samples: measurements, newest first, has to be longer than first sample by N+1
 # first_sample: the index of the oldest sample to calculate (10 means calculate 10 EMAs for samples 0..10)
+# returns: a list of emas from 0th sample to first_sample, oldest first (reverse input order)
 def ema_calculate(N: int, samples: list[float], first_sample: int) -> list[float]:
     sample_count: Final[float] = N+1
     alpha: Final[float] = ema_alpha(N)
@@ -57,24 +58,32 @@ def ema_calculate(N: int, samples: list[float], first_sample: int) -> list[float
 if __name__ == "__main__":
     left_ema_N = 12
     right_ema_N = 26
+    signal_ema_N = 9
 
     # read the prices from file
     data = pd.read_csv("HistoricalPrices.csv", skipinitialspace=True)
     dates = data["Date"]
     prices = list(data["Open"])
-
     # days in our data
-    day_count = len(prices)
-    # first possible to calculate (last by date)
+    day_count: Final[int] = len(prices)
     first_day = day_count - max(left_ema_N + 1, right_ema_N + 1)
 
+    # calculate the MACD
     left_emas = ema_calculate(left_ema_N, prices, first_day)
     right_emas = ema_calculate(right_ema_N, prices, first_day)
     macd_list = [(l - r) for (l,r) in zip(left_emas,right_emas)]
 
+    # calculate the signal line
+    first_day_signal = len(macd_list) - (signal_ema_N + 1)
+    signal_emas = ema_calculate(signal_ema_N, list(reversed(macd_list)),first_day_signal)
+
+    # pad the signal line at the beginning to align it with the MACD
+    signal_emas = ([signal_emas[0]] * signal_ema_N) + signal_emas
+
     fig = plt.figure()
     ax = plt.axes()
     ax.plot(macd_list)
+    ax.plot(signal_emas)
     fig.show()
 
     input()
